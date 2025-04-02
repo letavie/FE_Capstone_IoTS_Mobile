f8dd90b605fa5bc8cd9e1b5a14a0132d7fb1d029
vietlucoder <vietleruoi@gmail.com>
2025-04-02 20:53:14 +0700

    setup nativewind

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,25 @@
+import { Image, StyleSheet, Platform, View, Text } from "react-native";
+
+import { HelloWave } from "@/components/HelloWave";
+import ParallaxScrollView from "@/components/ParallaxScrollView";
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function HomeScreen() {
+  return (
+    <ParallaxScrollView
+      headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
+      headerImage={
+        <Image source={require("@/assets/images/partial-react-logo.png")} />
+      }
+    >
+      <SafeAreaView>
+        <View>
+          <Text className="text-red-300 font-bold bg-white ">test ne 3</Text>
+        </View>
+        <View className="viewBox"></View>
+      </SafeAreaView>
+    </ParallaxScrollView>
+  );
+}
